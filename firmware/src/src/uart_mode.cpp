dd5614f7f75de955407b42fe3461aa92e92d7957
PauliusMorku <paulius.morku@gmail.com>
2025-06-26 14:13:05 +0000

    change rf mode to 500

--- firmware/src/src/uart_mode.cpp
+++ firmware/src/src/uart_mode.cpp
@@ -226,7 +226,7 @@ void UartSetChannels(uint16_t channels[16])
       crsfout.PackedRCdataOut.ch13 = US_to_CRSF(channels[13]);
       crsfout.PackedRCdataOut.ch14 = US_to_CRSF(channels[14]);
       crsfout.PackedRCdataOut.ch15 = US_to_CRSF(channels[15]);
-      crsfout.LinkStatistics.rf_Mode = RATE_LORA_100HZ_8CH;
+      crsfout.LinkStatistics.rf_Mode = RATE_FLRC_500HZ;
       break;
     default:
       break;
